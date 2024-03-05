8da1dda6b0982770810095ca2f5fef61d5c1d615
jgari008 <jgari008@DESKTOP-ILSIF5E>
2024-03-05 13:13:52 -0700

    update comments endpoint and middleware

--- server/auth/middleware.js
+++ server/auth/middleware.js
@@ -2,20 +2,21 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function authenticateUser(req, res, next) {
-    const auth = req.headers.authorization;
-    const token = auth?.startsWith("Bearer ") ? auth.slice(7) : null;
-    try {
-        if (token) {
-            req.user = jwt.verify(token, process.env.JWT_SECRET);
-        } else {
-            req.user = null;
-        }
-    } catch (error) {
-        console.error("JWT verification error:", error);
-    }
+  const auth = req.headers.authorization;
+  const token = auth?.startsWith("Bearer ") ? auth.slice(7) : null;
+  if (!token) {
+    return res.status(401).send("Access denied. No token provided.");
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Assuming your JWT payload includes user details such as `id`
     next();
+  } catch (error) {
+    console.error("JWT verification error:", error);
+    return res.status(403).send("Invalid token.");
+  }
 }
 
 module.exports = {
-    authenticateUser,
+  authenticateUser,
 };
